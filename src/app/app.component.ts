ccbdc989420af703943ae4c3a1b48d4a8da3014a
sixmatom <sixmatom@gmail.com>
2024-07-22 14:32:08 +0200

    added details page housing service and routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {HomeComponent} from "./home/home.component";
-
+import {RouterModule} from "@angular/router";
 @Component({
   standalone: true,
   selector: 'app-root',
@@ -11,12 +11,12 @@ import {HomeComponent} from "./home/home.component";
              alt="logo" aria-hidden="true">
       </header>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>`,
   styleUrls: ['./app.component.css'],
   imports: [
-    HomeComponent
+    HomeComponent, RouterModule
   ]
 })
 export class AppComponent {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HousingLocationComponent} from "../housing-location/housing-location.component";
+import {HousingLocation} from "../housing-location"
+import { HousingService} from "../housing.service";
 
 @Component({
   selector: 'app-home',
@@ -10,15 +12,21 @@ import {HousingLocationComponent} from "../housing-location/housing-location.com
     <section>
       <form>
         <input type="text" placeholder="Filter by city">
-        <button class="priomary" type="button">Search</button>
+        <button class="primary" type="button">Search</button>
       </form>
     </section>
     <section class="results">
-      <app-housing-location></app-housing-location>
+      <app-housing-location *ngFor="let housingLocation of 
+      housingLocationList" [housingLocation]="housingLocation"></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+housingLocationList: HousingLocation[] = [];
+housingService: HousingService = inject(HousingService);
 
+constructor() {
+  this.housingLocationList = this.housingService.getAllHousingLocations();
+}
 }
